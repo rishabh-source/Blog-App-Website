d0d7823115b2211c2b257011718f3489dd12bd52
Rishabh <rg763652@gmail.com>
2024-10-14 10:45:06 +0530

    updated url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://blog-app-website.onrender.com",
+    origin:"https://blog-app-website-nu.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
