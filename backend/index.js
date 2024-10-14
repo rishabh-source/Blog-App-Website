b8d06a8957b53a39729e2168c9b4da5123046969
Rishabh <rg763652@gmail.com>
2024-10-14 11:10:43 +0530

    updated url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://blog-app-website.onrender.com",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -41,7 +41,7 @@ function Register() {
     formData.append("photo", photo);
     try {
       const { data } = await axios.post(
-        "http://localhost:4001/api/users/register",
+        "https://blog-app-website.onrender.com/api/users/register",
         formData,
         {
           withCredentials: true,
